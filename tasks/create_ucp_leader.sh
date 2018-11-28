dbe93fc2150541305909ce9dbb8e11f658d41c0c
Jeffrey Frelick <jeff@autostructure.io>
2018-11-28 16:52:12 -0500

    making docker command non-interactive

--- tasks/create_ucp_leader.sh
+++ tasks/create_ucp_leader.sh
@@ -7,5 +7,5 @@
 #
 my_fqdn=`hostname -f`
 my_ip=`facter ipaddress`
-docker container run --rm -it --name ucp-leader -v /var/run/docker.sock:/var/run/docker.sock $PT_docker_image install --force-minimums --tty --host-address $my_ip --san $my_fqdn --pod-cidr $PT_pod_cidr --admin-username $PT_ucp_admin_username --admin-password $PT_ucp_admin_password
+docker container run --rm --name ucp-leader -v /var/run/docker.sock:/var/run/docker.sock $PT_docker_image install --force-minimums --host-address $my_ip --san $my_fqdn --pod-cidr $PT_pod_cidr --admin-username $PT_ucp_admin_username --admin-password $PT_ucp_admin_password
 #docker container run --rm -it --name ucp-leader -v /var/run/docker.sock:/var/run/docker.sock $PT_docker_image install --force-minimums --host-address $my_ip --san $my_fqdn --pod-cidr $PT_pod_cidr --admin-username $PT_ucp_admin_username --admin-password $PT_ucp_admin_password
