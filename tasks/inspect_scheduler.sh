ecafa339a4e14222dd3cf835226e842ab667ad21
Jeffrey Frelick <jeff@autostructure.io>
2018-11-29 16:15:32 -0500

    added verbiage to output

--- tasks/inspect_scheduler.sh
+++ tasks/inspect_scheduler.sh
@@ -5,4 +5,5 @@
 # @Usage
 # Run this task on any node to output scheduler settings.
 #
+echo 'scheduler settings for node: '$PT_node
 docker node inspect $PT_node | grep -i orchestrator | tr -s [:space:]
