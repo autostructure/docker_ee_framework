9b6ab0daa9c0c2af7cf1380bbd9744403846d635
Jeffrey Frelick <jeff@autostructure.io>
2018-12-14 15:26:00 -0500

    nfs changes

--- examples/kubectl/nfs/kubectl-commands.sh
+++ examples/kubectl/nfs/kubectl-commands.sh
@@ -1,18 +1,25 @@
+# First, designate a node to host nfs by assigning it a role.
+# This is used by the Deployment's nodeSelector
+# Run this command after replacing worker001 with your nfs node name...
+# kubectl label node worker001 node-role.kubernetes.io/nfs-server=nfs-server
+
 kubectl create -f nfs-server.yaml
 
-kubectl describe pod nfs-server | grep IP:
+# echo "Open nfs-storage.yaml"
+# echo "Assign the nfs-server IP address (found above)"
+# echo "to the 'server:' parameter..."
+# pause
+# vi nfs-storage.yaml
 
-echo "Open nfs-storage.yaml"
-echo "Assign the nfs-server IP address (found above)"
-echo "to the 'server:' parameter..."
-
-pause
-
-vi nfs-storage.yaml
-
+echo "Assigning nfs-server IP address to storageClass server parameter."
+# cp nfs-storage.yaml nfs-storage.yaml.template
+export POD_IP=`kubectl describe pod nfs-server | grep IP: | grep -oE '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'`
+sed "s/replace-with-nfs-server-pod-ip-address/${POD_IP}/g" nfs-storage.yaml.template > nfs-storage.yaml
 kubectl create -f nfs-storage.yaml
+cp nfs-storage.yaml.template nfs-storage.yaml
+# rm nfs-storage.yaml.template
 
-kubectl create -f nfs-volume.yaml
+# kubectl create -f nfs-volume.yaml
 
 kubectl create -f nfs-claim.yaml
 
