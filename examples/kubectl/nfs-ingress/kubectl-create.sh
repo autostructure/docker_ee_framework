fc50eadac9ab62cd36a5439cfcde8ce004b6177b
Jeffrey Frelick <jeff@autostructure.io>
2018-12-20 15:16:18 -0500

    pvc worked...now figuring out where nfs dir is on nfs server

--- examples/kubectl/nfs-ingress/kubectl-create.sh
+++ examples/kubectl/nfs-ingress/kubectl-create.sh
@@ -10,11 +10,11 @@ kubectl create -f nfs-storage.yaml
 kubectl create -f nfs-volume.yaml
 kubectl create -f nfs-claim.yaml
 
-# kubectl create -f nginx-webserver-deployment.yaml
-# kubectl create -f nginx-webserver-service.yaml
-# kubectl create -f ingress-resource.yaml
-# kubectl create -f ingress-default-backend.yaml
-# kubectl create -f ingress-controller.yaml
+kubectl create -f nginx-webserver-deployment.yaml
+kubectl create -f nginx-webserver-service.yaml
+kubectl create -f ingress-resource.yaml
+kubectl create -f ingress-default-backend.yaml
+kubectl create -f ingress-controller.yaml
 
 
 
