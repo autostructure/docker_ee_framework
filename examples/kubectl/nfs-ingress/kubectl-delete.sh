c5f029da8124f287299f9887be51c3f8285eb8ac
Jeffrey Frelick <jeff@autostructure.io>
2018-12-20 16:55:33 -0500

    closer to nfs resolution.  something needs to be done with the vfs.conf

--- examples/kubectl/nfs-ingress/kubectl-delete.sh
+++ examples/kubectl/nfs-ingress/kubectl-delete.sh
@@ -25,10 +25,10 @@ kubectl delete pvc nfs-claim-1g
 # low-level resources with no namespaces...
 kubectl delete pv nfs-vol-autostructure-development
 kubectl delete pv nfs-vol-default
-# kubectl delete storageclass nfs-storage
+kubectl delete storageclass nfs-storage
 
 # nfs-servers...
-# kubectl delete pod nfs-server --namespace autostructure-development
+kubectl delete pod nfs-server --namespace autostructure-development
 # kubectl delete pod nfs-server
 
 # namespaces...
