18ba90c6d0c4e0d65d03eb89bee62036993aba54
Jeffrey Frelick <jeff@autostructure.io>
2019-01-04 13:40:23 -0500

    fixing nfs

--- examples/terraform/single-node/output-ips.tf
+++ examples/terraform/single-node/output-ips.tf
@@ -2,9 +2,9 @@
 # to a value greater than the default of 5 minutes.
 # You will find the setting in the vars file.
 
-output "workers-ip" {
-    value = "${vsphere_virtual_machine.docker_ucp_worker_nodes.*.default_ip_address}"
-}
+# output "workers-ip" {
+#     value = "${vsphere_virtual_machine.docker_ucp_worker_nodes.*.default_ip_address}"
+# }
 
 output "nfs-ip" {
     value = "${vsphere_virtual_machine.nfs.default_ip_address}"
