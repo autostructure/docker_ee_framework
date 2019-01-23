29a1d26aa4eeb31e9d13fdb4c119192268ccf87d
Jeffrey Frelick <jeff@autostructure.io>
2019-01-23 12:26:16 -0500

    just added some comments and a cmd argument

--- examples/kubernetes/nfs-ingress/kubectl-get.sh
+++ examples/kubernetes/nfs-ingress/kubectl-get.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# troubleshooting...
+# 
+# kubectl get pods --all-namespaces --show-all
+# kubectl get pod nginx-ingress-controller --namespace=ci-cd -o yaml --export
+#
+# kubectl get pod cdpe-cdpe-0 --namespace=ci-cd -o yaml --export
+# kubectl get pod cdpe-mysql-5fd94f5855-8swv2 --namespace=ci-cd -o yaml --export
+# kubectl get pod cdpe-mysql-5fd94f5855-j4qtd --namespace=ci-cd -o yaml --export
+#
+# kubectl describe pod cdpe-cdpe-0 --namespace=ci-cd
+# kubectl describe pod cdpe-mysql-5fd94f5855-8swv2 --namespace=ci-cd
+# kubectl describe pod cdpe-mysql-5fd94f5855-j4qtd --namespace=ci-cd
+
 clear
 echo "Processes..."
 kubectl get po -o wide --all-namespaces
@@ -17,7 +30,7 @@ echo "PersistentVolumeClaims..."
 kubectl get pvc --all-namespaces
 echo
 echo "Pods..."
-kubectl get pods -o wide --all-namespaces
+kubectl get pods -o wide --all-namespaces --show-all
 echo
 echo "Deployments..."
 kubectl get deployment --all-namespaces
